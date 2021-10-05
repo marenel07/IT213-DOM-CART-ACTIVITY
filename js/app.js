62f01e2a641f4342cc66f0c5b4e56dbda00e1957
marenel07 <villaralbonel29@gmail.com>
2021-10-04 21:41:18 -0700

    Print from Storage on load to app.js

--- js/app.js
+++ js/app.js
@@ -20,6 +20,9 @@ function loadEventListeners() {
     //Clear Cart Btn
     clearCartBtn.addEventListener('click', clearCart);
 
+    // Document ready
+    document.addEventListener('DOMContentLoaded', getFromLocalStorage);
+
 
 }
 
@@ -104,7 +107,7 @@ function getCoursesFromStorage() {
     if(localStorage.getItem('courses') === null) {
         courses = [];
     } else {
-        courses = JSON.parse(localStorage.getItem('course') );
+        courses = JSON.parse(localStorage.getItem('courses') );
     }
     return courses;
 }
@@ -126,6 +129,38 @@ function clearCart() {
 
     }
 
+}
+
+//Loads when document is ready and print courses into shopping cart
+
+function getFromLocalStorage() {
+    let coursesLS = getCoursesFromStorage();
+
+    //LOOP through the courses and print into the cart
+    coursesLS.forEach(function(course) {
+        //create the <tr>
+        const row = document.createElement('tr');
+
+        //print the content
+        row.innerHTML = `
+             <tr>
+                 <td>
+                     <img src="${course.image}" width=100>
+                 </td>
+                 <td>${course.title}</td>
+                 <td>${course.price}</td>
+                 <td>
+                    <a href="#" class="remove" data-id="${course.id}">X</a>
+                 </td>
+
+
+             </tr>
+
+        
+        `;
+        shoppingCartContent.appendChild(row);
+
+    });
 }
 
 
